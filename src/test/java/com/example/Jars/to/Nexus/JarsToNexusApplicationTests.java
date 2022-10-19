b2cda3c2d8ba6f932ee6ffe8fdc582ed5da07367
balajis <balajis@ADFL-1210.ADFDATA.NET>
2022-10-19 12:12:21 +0530

    First commit

---
+++ src/test/java/com/example/Jars/to/Nexus/JarsToNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jars.to.Nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarsToNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
